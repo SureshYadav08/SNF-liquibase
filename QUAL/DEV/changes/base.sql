e0bd7d6a5de4cbde37697f985d700946ffb3a461
SureshYadav08 <145325912+SureshYadav08@users.noreply.github.com>
2025-04-22 11:51:20 +0530

    Update base.sql

--- QUAL/DEV/changes/base.sql
+++ QUAL/DEV/changes/base.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset gajjisuresh08@gmail.com:basetable.sql1
+--changeset gajjisuresh08@gmail.com:basetable.sql2
 
 CREATE TABLE IF NOT EXISTS  TEST_B (
     ID NUMBER,
